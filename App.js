32749643ae82f316ae1c1a26cab2d4dbe7bf71b7
developer abir <developerabir40@gmail.com>
2022-09-23 20:30:16 +0600

    rn-tw-navigati

---
+++ App.js
@@ -0,0 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text className="text-3xl">
+        Hello I am abir khan I have great challange to start react native 356
+        day challange
+      </Text>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './App.{js,jsx,ts,tsx}',
+    './screen/**/*.{js,jsx,ts,tsx}',
+    './pages/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
